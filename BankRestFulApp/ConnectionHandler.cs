35241c99c78783d0dc1205b153260421937bbb70
Abimbola <craftwordltd@gmail.com>
2019-11-21 18:52:29 +0100

    add the connection handler class

---
+++ BankRestFulApp/ConnectionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace BankRestFulApp
+{
+    public class ConnectionHandler
+    {
+        static SqlConnection con = null;
+        public static SqlConnection ConnectObj
+        {
+            get
+            {
+                if (con == null)
+                    con = new SqlConnection("Data Source=DESKTOP-1CH1I23;Initial Catalog=BankApp;Integrated Security=True");
+                return con;
+            }
+        }
+    }
+}
